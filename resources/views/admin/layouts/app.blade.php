12db01d3ea1ff7b2c8a40e20b1130a40e836df46
hridoysheikh15 <ridoysheikh15@gmail.com>
2025-02-14 01:09:23 +0600

    CDN downloaded and add as local for better experience

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,36 +4,35 @@
 <head>
     <meta charset="utf-8">
     {{-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> --}}
-    <title>Ela Admin - @yield('title')</title>
-    {{-- <meta name="description" content="Ela Admin - HTML5 Admin Template"> --}}
+    <title>CHTS Admin - @yield('title')</title>
+    {{-- <meta name="description" content="CHTS Admin - HTML5 Admin Template"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
     <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
-    <link rel="stylesheet" href="{{ url('assets/css/cs-skin-elastic.css') }}">
-    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/cdn_css/normalize.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/cdn_css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/font_awesome/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/themify/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/pixeden/pe-icon-7-stroke.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/fonts/flag_icon/flag-icon.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/cs-skin-elastic.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
-    <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/cdn_css/chartist.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/cdn_css/jqvmap.min.css') }}">
 
-    <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{ asset('assets/css/cdn_css/weather-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/cdn_css/fullcalendar.min.css') }}">
     
     {{-- toster css --}}
-    <link rel="stylesheet" href="{{url('assets/css/toaster.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/toaster.css') }}">
 
     {{-- select 2 --}}
-    <link rel="stylesheet" href="{{url('assets/css/lib/chosen/chosen.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/chosen/chosen.min.css') }}">
 
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="{{ asset('assets/js/jquery-3.7.1.min.js') }}"></script>
     <style>
         /* for tab padding */
         .nav-padding {
@@ -76,36 +75,34 @@
 
 
     <!-- Scripts -->
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
-    <script src="{{ url('assets/js/main.js') }}"></script>
+    <script src="{{ asset('assets/js/cdn_js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/cdn_js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/cdn_js/jquery.matchHeight.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
 
     <!--  Chart js -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.3/dist/Chart.bundle.min.js"></script>
+    <script src="{{ asset('assets/js/cdn_js/Chart.bundle.min.js') }}"></script>
 
     <!--Chartist Chart-->
-    <script src="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartist-plugin-legend@0.6.2/chartist-plugin-legend.min.js"></script>
+    <script src="{{ asset('assets/js/cdn_js/chartist.min.js') }}"></script>
+    <script src="{{ asset('assets/js/cdn_js/chartist-plugin-legend.min.js') }}"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/jquery.flot@0.8.3/jquery.flot.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/flot-pie@1.0.0/src/jquery.flot.pie.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/flot-spline@0.0.1/js/jquery.flot.spline.min.js"></script>
+    <script src="{{ asset('assets/js/cdn_js/jquery.flot.min.js') }}"></script>
+    <script src="{{ asset('assets/js/cdn_js/jquery.flot.pie.min.js') }}"></script>
+    <script src="{{ asset('assets/js/cdn_js/jquery.flot.spline.min.js') }}"></script>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/moment@2.22.2/moment.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.js"></script>
-    <script src="{{ url('assets/js/init/fullcalendar-init.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('assets/js/cdn_js/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/js/cdn_js/fullcalendar.min.js') }}"></script>
+    <script src="{{ asset('assets/js/init/fullcalendar-init.js') }}"></script>
+    <script src="{{ asset('assets/js/cdn_js/sweetalert2@11.js') }}"></script>
 
     <!-- select 2 -->
-    <script src="{{url('assets/js/lib/chosen/chosen.jquery.min.js')}}"></script>
+    <script src="{{ asset('assets/js/lib/chosen/chosen.jquery.min.js') }}"></script>
 
     <!-- toster js -->
-    <script src="{{url('assets/js/toaster.js')}}"></script>
+    <script src="{{ asset('assets/js/toaster.js') }}"></script>
     <script>
         $(document).ready(function() {
             $(
